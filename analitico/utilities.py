573b9ab15a55fbc824fe5056e929d09d4b42e27b
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-11 21:08:09 +0100

    Add utilities for date augmentation in pd.DataFrame

--- analitico/utilities.py
+++ analitico/utilities.py
@@ -6,19 +6,19 @@ import numpy as np
 import pandas as pd
 import json
 import logging
-import holidays
 
-from catboost import Pool
 from datetime import datetime
 
 # default logger for analitico's libraries
 logger = logging.getLogger("analitico")
 
-__all__ = ["timestamp_to_time", "augment_timestamp_column", "dataframe_to_catpool"]
+##
+## Json utilities
+##
 
 
 def save_json(data, filename, indent=4):
-    """ Saves given data in a json file (prettified by default) """
+    """ Saves given data in a json file (we love pretty, so prettified by default) """
     with open(filename, "w") as f:
         json.dump(data, f, indent=indent)
 
@@ -34,6 +34,11 @@ def read_json(filename, encoding="utf-8"):
         raise Exception(detail, exc)
 
 
+##
+## Time utilities
+##
+
+
 def time_ms(started_on=None):
     """ Returns the time elapsed since given time in ms """
     return datetime.now() if started_on is None else int((datetime.now() - started_on).total_seconds() * 1000)
@@ -46,8 +51,9 @@ def time_it(code):
     return int((datetime.now() - started_on).total_seconds() * 1000)
 
 
-# holiday calendar
-_it_holidays = holidays.Italy()
+##
+## Timestamp utilities
+##
 
 
 def timestamp_to_time(ts: str, ts_format="%Y-%m-%d %H:%M:%S") -> time.struct_time:
@@ -69,100 +75,9 @@ def timestamp_diff_secs(ts1, ts2):
     return t1 - t2
 
 
-def augment_timestamp_columnOLD(df: pd.DataFrame, col: str, ts_format="%Y-%m-%d %H:%M:%S") -> pd.DataFrame:
-    """ Expand a timestamp column into a number of separate columns
-        with the year, month [1,12], hour [0,23], minute [0,59], 
-        weekday [0,6] and day of the year [1,366]. The name of the 
-        new columns follows the name of the original column with
-        the _year, _month, _hour, _min, _wday and _yday suffixes appended. """
-    # https://docs.python.org/3/library/time.html#module-time
-    df[col + "_year"] = df[col].map(lambda ts: timestamp_to_time(ts, ts_format).tm_year)
-    df[col + "_month"] = df[col].map(lambda ts: timestamp_to_time(ts).tm_mon)
-    df[col + "_day"] = df[col].map(
-        lambda ts: timestamp_to_time(ts).tm_mday
-    )  # .astype(CategoricalDtype(range(1,31),ordered=True))
-    df[col + "_hour"] = df[col].map(lambda ts: timestamp_to_time(ts).tm_hour)
-    df[col + "_min"] = df[col].map(lambda ts: timestamp_to_time(ts).tm_min)
-    df[col + "_weekday"] = df[col].map(
-        lambda ts: timestamp_to_time(ts).tm_wday
-    )  # .astype(CategoricalDtype(range(0,6),ordered=True))
-    df[col + "_yearday"] = df[col].map(
-        lambda ts: timestamp_to_time(ts).tm_yday
-    )  # .astype(CategoricalDtype(range(1,366),ordered=True))
-    df[col + "_holyday"] = df[col].map(lambda ts: int(ts[:10] in _it_holidays))
-    return df
-
-
-def augment_timestamp_column(df: pd.DataFrame, col: str, ts_format="%Y-%m-%d %H:%M:%S") -> pd.DataFrame:
-    """ Expand a timestamp column into a number of separate columns
-        with the year, month [1,12], hour [0,23], minute [0,59], 
-        weekday [0,6] and day of the year [1,366]. The name of the 
-        new columns follows the name of the original column with
-        the _year, _month, _hour, _min, _wday and _yday suffixes appended. """
-    # https://docs.python.org/3/library/time.html#module-time
-
-    # use empty string for categorical values when value is not known
-    df[col + "_year"] = ""
-    df[col + "_month"] = ""
-    df[col + "_day"] = ""
-    df[col + "_hour"] = ""
-    df[col + "_min"] = ""
-    df[col + "_weekday"] = ""
-    df[col + "_yearday"] = ""
-    df[col + "_holyday"] = ""
-
-    for idx, row in df.iterrows():
-        t = timestamp_to_time(row[col], ts_format)
-        if t:
-            df.at[idx, col + "_year"] = t.tm_year
-            df.at[idx, col + "_month"] = t.tm_mon
-            df.at[idx, col + "_day"] = t.tm_mday  # .astype(CategoricalDtype(range(1,31),ordered=True))
-            df.at[idx, col + "_hour"] = t.tm_hour
-            df.at[idx, col + "_min"] = t.tm_min
-            df.at[idx, col + "_weekday"] = t.tm_wday  # .astype(CategoricalDtype(range(0,6),ordered=True))
-            df.at[idx, col + "_yearday"] = t.tm_yday  # .astype(CategoricalDtype(range(1,366),ordered=True))
-            df.at[idx, col + "_holyday"] = int(row[col][:10] in _it_holidays)
-    return df
-
-
-def dataframe_to_catpool(
-    df: pd.DataFrame, features, categorical_features=None, timestamp_features=None, label_feature=None
-):
-    """ Takes a pandas dataframe and prepares a catboost pool to be used for training or prediction.
-        df: The source dataframe
-        features: An array of names of columns to be used as features
-        categorical_features: An array of names of categorical features
-        timestamp_features: Timestamp features will be augmented
-        label_feature: If specified, the labels (eg: regression targets) are extracted and returned
-        Returns: A catboost Pool and the column with the labels (if specified)
-    """
-    categorical_features = categorical_features.copy()
-    df = df.copy()
-    # save labels column
-    df_labels = df[label_feature] if label_feature is not None else None
-    # reorder columns, drop unused
-    df = df[features]
-
-    for categorical_feature in categorical_features:
-        df[categorical_feature].astype(str)
-        df = df.fillna(value={categorical_feature: ""})
-
-    # augment timestamps
-    if timestamp_features is not None:
-        for timestamp_feature in timestamp_features:
-            df = augment_timestamp_column(df, timestamp_feature)
-            df = df.drop(columns=timestamp_feature)
-            categorical_features.append(timestamp_feature + "_year")
-            categorical_features.append(timestamp_feature + "_month")
-            categorical_features.append(timestamp_feature + "_day")
-            categorical_features.append(timestamp_feature + "_weekday")
-            categorical_features.append(timestamp_feature + "_holiday")
-
-    # indexes of columns with categorical features
-    categorical_idx = [df.columns.get_loc(c) for c in df.columns if c in categorical_features]
-    df2 = df.copy()
-    pool = Pool(df2, df_labels, cat_features=categorical_idx)
-    return pool, df_labels
+##
+## Dictionary utilities
+##
 
 
 def get_dict_dot(d: dict, key: str, default=None):
@@ -191,3 +106,34 @@ def set_dict_dot(d: dict, key: str, value=None):
         if not (subkey in d):
             d[subkey] = None
         set_dict_dot(d[subkey], key[len(subkey) + 1 :], value)
+
+
+##
+## Pandas utilities
+##
+
+
+def pd_cast_datetime(df, column):
+    """ Casts a string column to a date column, assumes format is recognizable """
+    df[column] = pd.to_datetime(df[column], infer_datetime_format=True, errors="coerce")
+
+
+def pd_augment_date(df, column):
+    """ Splits a datetime column into year, month, day, dayofweek, hour, minute then removes the original column """
+    loc = df.columns.get_loc(column) + 1
+    # create separate columns for each parameter
+    df.insert(loc, column + ".minute", df[column].apply(lambda ts: ts.minute))
+    df.insert(loc, column + ".hour", df[column].apply(lambda ts: ts.hour))
+    df.insert(loc, column + ".day", df[column].apply(lambda ts: ts.day))
+    df.insert(loc, column + ".month", df[column].apply(lambda ts: ts.month))
+    df.insert(loc, column + ".year", df[column].apply(lambda ts: ts.year))
+    df.insert(loc, column + ".dayofweek", df[column].apply(lambda ts: ts.dayofweek))
+    df.drop([column], axis=1, inplace=True)
+
+
+def pd_timediff_min(df, column_start, column_end, column_diff):
+    """ Creates a new column with difference in minutes between the two named columns """
+    pd_cast_datetime(df, column_start)
+    pd_cast_datetime(df, column_end)
+    df[column_diff] = df[column_end] - df[column_start]
+    df[column_diff] = df[column_diff].dt.total_seconds() / 60.0
