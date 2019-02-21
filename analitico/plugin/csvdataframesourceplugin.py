00ff01111202388161967d4663b39718979aa836
Gionata Mettifogo <gmettifogo@gmail.com>
2019-02-21 13:09:02 +0100

    Force encoding while reading csv files

--- analitico/plugin/csvdataframesourceplugin.py
+++ analitico/plugin/csvdataframesourceplugin.py
@@ -59,7 +59,7 @@ class CsvDataframeSourcePlugin(IDataframeSourcePlugin):
                         index = column["name"]
 
             stream = self.factory.get_url_stream(url)
-            df = pandas.read_csv(stream, dtype=dtype, parse_dates=parse_dates)
+            df = pandas.read_csv(stream, dtype=dtype, parse_dates=parse_dates, encoding="utf-8")
 
             if index:
                 # transform specific column with unique values to dataframe index
