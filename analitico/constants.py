fa82fb93713be9fb577aed0352da5d12e5d7b812
Gionata Mettifogo <gmettifogo@gmail.com>
2019-03-07 12:51:21 +0100

    Add plugin constants

--- analitico/constants.py
+++ analitico/constants.py
@@ -16,20 +16,22 @@ TYPE_PREFIX = "analitico/"
 DATASET_TYPE = "dataset"
 ENDPOINT_TYPE = "endpoint"
 JOB_TYPE = "job"
+LOG_TYPE = "log"
 MODEL_TYPE = "model"
+PLUGIN_TYPE = "plugin"
 RECIPE_TYPE = "recipe"
 TOKEN_TYPE = "token"
 USER_TYPE = "user"
 WORKSPACE_TYPE = "workspace"
-LOG_TYPE = "log"
 
 # IDs
 DATASET_PREFIX = "ds_"  # dataset source, filters, etc
 ENDPOINT_PREFIX = "ep_"  # inference endpoint configuration
 JOB_PREFIX = "jb_"  # sync or async job
+LOG_PREFIX = "lg_"  # log record
 MODEL_PREFIX = "ml_"  # trained machine learning model (not a django model)
 RECIPE_PREFIX = "rx_"  # machine learning recipe (an experiment with modules, code, etc)
 TOKEN_PREFIX = "tok_"  # authorization token
+PLUGIN_PREFIX = "pl_" # plugin instance (not plugin description or metadata)
 USER_PREFIX = "id_"  # an identity profile
 WORKSPACE_PREFIX = "ws_"  # workspace with rights and one or more projects and other resources
-LOG_PREFIX = "lg_"  # log record
